22ac34e3a5f48ee38dc6529969ad27fda424d0fc
pengyangchao <474114736@qq.com>
2017-12-01 09:22:39 +0800

    详情jquery

--- 京东项目/京东详情页/jd_detail.js
+++ 京东项目/京东详情页/jd_detail.js
@@ -1,3 +1,23 @@
-$('.app_jd').hover(function () {
-    
+$('.app_jd,.service').hover(function () {
+    $(this).children("[id$='items']").toggle()
+        .prev().toggleClass('hover');
+})
+$('#category').hover(function () {
+    $('#cate_box').toggle();
+})
+
+$('#cate_box').on('mouseenter','li',showsub).on('mouseleave','li',showsub);
+
+function showsub() {
+    $(this).children('.sub_cate_box').toggle().prev().toggleClass('hover');
+}
+
+$('#product_detail>.main_tabs').on('click','li:not(.current)',function () {
+    var $div= $("#product_detail>[id^='product']");
+    $div.removeClass('show');
+    $(this).addClass('current').siblings().removeClass('current');
+    if(!$(this).is(":contains('商品评价')")){
+        var i=$(this).attr('i');
+        $("#product_detail>[id^='product']").eq(i).addClass('show').siblings().removeClass('show');
+    }
 })
