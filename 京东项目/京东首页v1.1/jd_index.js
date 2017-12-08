adddfebd5efcabb838680d12f6af5f62aff8a219
pengyangchao <474114736@qq.com>
2017-12-08 19:16:07 +0800

    京东首页v1.1

---
+++ 京东项目/京东首页v1.1/jd_index.js
@@ -0,0 +1,14 @@
+/*封装$*/
+window.$=HTMLElement.prototype.$=function(selector){
+    var elems=(this==window?document:this)
+        .querySelectorAll(selector);
+    return elems.length==0?null:elems.length==1?elems[0]:elems;
+}
+/*广告图片数组*/
+var imgs=[
+	{"i":0,"img":"images/index/banner_01.jpg"},
+    {"i":1,"img":"images/index/banner_02.jpg"},
+    {"i":2,"img":"images/index/banner_03.jpg"},
+    {"i":3,"img":"images/index/banner_04.jpg"},
+    {"i":4,"img":"images/index/banner_05.jpg"},
+];
