63f47ba8274b2c6497798036c7aeae6c9932d379
pengyangchao <474114736@qq.com>
2018-01-17 11:34:25 +0800

    ~

--- 珠峰js总结/小项目/js/utils.js
+++ 珠峰js总结/小项目/js/utils.js
@@ -1,7 +1,7 @@
 var utils={
    	//用try catch 方法让类数组变数组兼容所有浏览器
-   		listToArray:function(likeArray){
-   			try{
+     listToArray:function(likeArray){
+      try{
    				// 标准
    				var arr= Array.prototype.slice.call(likeArray);
    			}
@@ -15,8 +15,31 @@ var utils={
    			return arr;
    		},
 
-   		//ie6、7下没有JSON对象，json字符串转化为json对象兼容方法；
-   		jsonParse:function(jsonString){
-   			return "JSON" in window ? JSON.parse(jsonString) : eval("("+jsonString+")");
-   		}
-   	}
+		//ie6、7下没有JSON对象，json字符串转化为json对象兼容方法；
+		jsonParse:function(jsonString){
+			return "JSON" in window ? JSON.parse(jsonString) : eval("("+jsonString+")");
+		},
+
+      getCss:function(curEle,attr){
+         var val=null; var reg=null;
+
+         if('getComputedStyle' in window){
+            val=getComputedStyle(curEle,null)[attr] box
+         }else{
+         //ie6-8不支持opacity 只支持滤镜；
+         if(attr=='opacity'){
+
+            val=curEle.currentStyle["filter"];
+            reg=/^alpha\(opacity=(\d+(?:\.\d+)?)\)$/i;
+            val=reg.test(val)?reg.exec(val)[1]/100:1;
+
+         }else{
+            val=curEle.currentStyle[attr];
+         }
+      }
+         //如果是px pt rem em 去单位计算；
+         reg=/^(-?\d+(\.\d+)?)(px|pt|rem|em)$/i;
+         return reg.test(val)?parseFloat(val):val;
+      },
+
+   }
