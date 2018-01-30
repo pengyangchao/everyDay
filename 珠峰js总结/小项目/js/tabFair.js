69a9633da32998c9e38ded99e882b4389128e606
pengyangchao <474114736@qq.com>
2018-01-31 00:00:34 +0800

    构造函数

--- 珠峰js总结/小项目/js/tabFair.js
+++ 珠峰js总结/小项目/js/tabFair.js
@@ -48,3 +48,53 @@
 }
 window.tabChange=tabChange;
 }();
+
+
+// 选项卡构造函数封装方法
+// @parmar container [object] 容器
+// @parmar defultIndex [num] 默认选中序号
+~function(){
+	function tabChange(container,defultIndex){
+		return this.init(container,defultIndex);
+	}
+	tabChange.prototype={
+		constructor:tabChange,
+		init:function(container,defultIndex){
+			this.container=container||null;
+			this.defultIndex=defultIndex||0;
+			this.tabfirst=utils.firstChild(this.container);
+			this.oLi=utils.children(this.tabfirst);
+			this.oDivs=utils.nextAll(this.tabfirst);
+			this.initDefault();
+			this.liveClick();
+			return this;
+		},
+		//初始化选中
+		initDefault:function(){
+			utils.addClass(this.oLi[this.defultIndex],'select');
+			utils.addClass(this.oDivs[this.defultIndex],'select');
+		},
+
+		liveClick:function(){
+			var _this=this;
+			this.tabfirst.onclick=function(e){
+				e=e||window.event;
+				e.target=e.target||e.srcElement;
+				if(e.target.nodeName.toLowerCase()=="li"){
+					var curLi=e.target;
+					curLi.index=utils.index(curLi);
+					_this.liDetail(curLi);
+				}
+			}
+		},
+		liDetail:function(curLi){
+			for(var i = 0;i<this.oLi.length;i++){
+				utils.removeClass(this.oLi[i],'select');
+				utils.removeClass(this.oDivs[i],'select');
+			}
+			utils.addClass(curLi,'select');
+			utils.addClass(this.oDivs[curLi.index],'select');
+		}
+	}
+	window.constructorTabChange=tabChange;
+}();
